f28fa25d0c3bc0eb25dff3defd0dac2732867e2c
Marcio <marciosut1996@gmail.com>
2019-09-20 22:50:13 -0300

    Adding the search field

--- client/src/Layout.js
+++ client/src/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { withRouter } from "react-router";
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Search from './components/Search';
 import "./App.scss";
 
 const Body = props => {
@@ -9,6 +10,7 @@ const Body = props => {
     <>
       <Header />
         <div className="page-content">
+        <Search />
           <main className="page-content__body">{props.children}</main>
         </div>
       <Footer />

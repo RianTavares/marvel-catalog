625379d2536a46b33410da14c023600a5e4ab8b9
Marcio Sardou <marciosut1996@gmail.com>
2019-10-04 17:27:21 -0300

    adding style to intern page

--- client/src/pages/Hq/index.js
+++ client/src/pages/Hq/index.js
@@ -5,26 +5,19 @@ const Hq = () => {
     
     return(
         <>
-        <section>
-            <div className="title">
-                <h1 className="title__text">
-                Vingadores - Guerra Infinita
-                </h1>
-            </div>
+            <section className="hq-details">
+                <h1 className="hq-details__title">Vingadores - Guerra Infinita</h1>
 
-            <div className="container">
-                <div className="container__hq-img"></div>
+                <div className="hq-details__img"></div>
                 
-                <div className="container__texts">
-                    <p className="description">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                </div>
+                <p className="hq-details__description">
+                    Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+                </p>
 
-                <p className="container__author">Author: MARCIO</p>
-            </div>
-
-        </section>
+                <p className="hq-details__author">Author: MARCIO</p>
+            </section>
         </>
-        );
-    }
+    );
+}
     
-    export default Hq;
+export default Hq;

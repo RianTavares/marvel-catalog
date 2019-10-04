32f05f4840a25bd5b10d19d0fcb0bd977d5fc83a
Marcio Sardou <marciosut1996@gmail.com>
2019-10-04 00:34:50 -0300

    testing fonts

--- client/src/pages/Hq/index.js
+++ client/src/pages/Hq/index.js
@@ -5,22 +5,24 @@ const Hq = () => {
     
     return(
         <>
-        <div className="title">
-            <h1 className="title__text">
-            Vingadores - Guerra Infinita
-            </h1>
-        </div>
-
-        <div className="container">
-            <div className="container__hq-img"></div>
-            
-            <div className="container__texts">
-                <p className="description">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+        <section>
+            <div className="title">
+                <h1 className="title__text">
+                Vingadores - Guerra Infinita
+                </h1>
             </div>
-        </div>
 
-            <p>Author: MARCIO</p>
+            <div className="container">
+                <div className="container__hq-img"></div>
+                
+                <div className="container__texts">
+                    <p className="description">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
+                </div>
 
+                <p className="container__author">Author: MARCIO</p>
+            </div>
+
+        </section>
         </>
         );
     }

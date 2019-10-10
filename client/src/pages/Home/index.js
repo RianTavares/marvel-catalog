00b67448e8f77d6e533b3e982631494a5057f900
Marcio Sardou <marciosut1996@gmail.com>
2019-10-10 02:17:22 -0300

    testing mock connection and starting api connection

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -4,29 +4,57 @@ import Item from "../../components/ItemHQ";
 
 const Home = () => {
 
-    const [backMsg, setBackMessage] = useState([]);
-
+    const [ backMsg, setBackMsg ] = useState([]);
+    
     const callApi = async () => {
-        const request = await fetch('api/v1/teste');
-        const data = await request.json();
-        setBackMessage(data);
-        console.log(data)
+        
+        const request = await fetch('api/v1/teste'); //pq
+        const data = await request.json();  
+        setBackMsg(data);
+        console.log(data);
     }
-
-    useEffect(()=>{
+    
+    useEffect (() => {
         callApi()
+        
     }, [])
+    
+    return (
+        <div>
+        <Search />           
+        <h1>{backMsg.map((response) => {        //pq
+            return(response.thumb)
+        })}</h1>
+        <Item />
+        </div>
+        )
+    }
+    
+    export default Home;
+ 
 
-    return(
-        <>
-            <Search />           
-            <h1>pagina principal</h1>
-            <Item />
-            <div>{backMsg.map((txt)=>{
-                return(txt.thumb)
-            })}</div>
-        </>
-    );
-}
-
-export default Home;
+       // const [backMsg, setBackMessage] = useState([]);
+    
+    // const callApi = async () => {
+    //     const request = await fetch('api/v1/teste');
+    //     const data = await request.json();
+    //     setBackMessage(data);
+    //     console.log(data)
+    //     alert("ativou")
+    // }
+    //     useEffect(()=>{
+    //         callApi()
+    //     }, [])
+    
+    //     return(
+    //         <>
+    //             <Search />           
+    //             <h1>pagina principal</h1>
+    //             <Item />
+    //             <div>{backMsg.map((txt)=>{
+    //                 return(txt.thumb)
+    //             })}</div>
+    //         </>
+    //     );
+    // }
+    
--- routes.js
+++ routes.js
@@ -7,65 +7,102 @@ dotenv.config();
 
 module.exports = () => {
     
+    //   MANDANDO PRO FRONT O DATA DO MOCK
     api.get('/teste', (req, res) => {
-        const response = []
-        // let count = 0
 
+        const response = [];
+        
         comics.data.results.map((item) => {
+
             const obj = {
                 "id": item.id,
                 "title": item.title,
                 "thumb": item.thumbnail.path + "." + item.thumbnail.extension 
             }
-            response.push(obj)
-            // console.log(count+=1);
-              
+            response.push(obj);
         })
-
-
-
         res.json(response);
     })
-    
-    api.get('/comics', (req, res) => {
-    
-        const BASE_URL = 'http://gateway.marvel.com/v1/public/comics?';
-        const limit = 'limit=10';
-        const TIME_STAMP = Date.now();
-        const API_KEY = process.env.API_KEY;
-        const PRIVATE_KEY = process.env.PRIVATE_KEY;
-        const mixin = TIME_STAMP + PRIVATE_KEY + API_KEY;
-        const HASH = md5(mixin);
-        let comicsArray = [];
-
-        axios.get(BASE_URL+limit+'&ts='+TIME_STAMP+'&apikey='+API_KEY+'&hash='+HASH)
-        .then(response => {
-
-            const results = response.data.data.results;
-
-            results.forEach(element => {
-                
-                let title = element.title;
-                title = title.slice(0, 11)
-                title = title + '...';
-        
-                const obj = {
-                    'title': title,
-                    'thumb': element.thumbnail.path + '.' + element.thumbnail.extension,
-                    'id': element.id
-                }
-                    
-                comicsArray.push(obj)
-            });
-            
-            res.json(comicsArray);
-
-        })
-        .catch(error => {
-            console.log(error);
-        });
-        
-    })
-
     return api;
 }
+    
+    
+
+
+
+
+    
+//CONSUILTANDO A API MARCIO  
+    //     api.get('/comics', (req, res) => {
+            
+    //         const BASE_URL = 'http://gateway.marvel.com/v1/public/comics?';
+    //         const limit = 'limit=10';
+    //         const TIME_STAMP = Date.now();
+    //         const API_KEY = process.env.API_KEY;
+    //         const PRIVATE_KEY = process.env.PRIVATE_KEY;
+    //         const mixin = TIME_STAMP + PRIVATE_KEY + API_KEY;
+    //         const HASH = md5(mixin);
+    //         let comicsArray = [];
+            
+            
+    //         axios.get(`${BASE_URL,limit} &ts= ${TIME_STAMP} &apikey= ${API_KEY} &hash= ${HASH}`)
+    //         .then(response => {
+                
+    //             const results = response.data.data.results;
+                
+    //             results.forEach((hq) =>{
+                    
+    //                 let title = hq.title;
+    //                 title = title.splice(0,11);
+    //                 title = title + '...';
+    
+    //                 const obj = {
+    //                     'id':hq.id,
+    //                     'title': title,
+    //                     'thumb': hq.thumb + "." + hq.extension
+    //                 }
+    //                 comicsArray.push(obj);
+    //             });
+    
+    //             res.json(comicsArray);
+    //         })
+    //         .catch((err) => console.log(`O erro é:${err}`));
+    //     })
+    //     return api;
+    // }
+    
+
+
+
+
+    //CONSULTANDO A  API RIAN
+    //     axios.get(BASE_URL+limit+'&ts='+TIME_STAMP+'&apikey='+API_KEY+'&hash='+HASH)
+    //     .then(response => {
+    
+    //         const results = response.data.data.results;
+    
+    //         results.forEach(element => {
+    
+    //             let title = element.title;
+    //             title = title.slice(0, 11)
+    //             title = title + '...';
+    
+    //             const obj = {
+    //                 'title': title,
+    //                 'thumb': element.thumbnail.path + '.' + element.thumbnail.extension,
+    //                 'id': element.id
+    //             }
+    
+    //             comicsArray.push(obj)
+    //         });
+    
+    //         res.json(comicsArray);
+    
+    //     })
+    //     .catch(error => {
+    //         console.log(error);
+    //     });
+    
+    // })
+
+    

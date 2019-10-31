580259384eb6cf7522f5b43bc5b025e48ac9b48e
riantavares <rian.tavares@live.com>
2019-10-31 19:55:09 -0300

    starting creating a search engine, but the onChange function is not working as expected

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,43 +1,62 @@
 import React, {useState, useEffect} from "react";
-import Search from "./../../components/Search";
 import Item from "../../components/ItemHQ";
 
 const Home = () => {
     
-    const [ backMsg, setBackMsg ] = useState([]);
+    const [backMsg, setBackMsg] = useState([]);
+    const [search, setSearch] = useState('');
+    const [test, setTest] = useState([]);
     
     const callApi = async () => {
-        
         const request = await fetch('api/v1/comics');
         const data = await request.json();  
         setBackMsg(data);
-
+        setTest(data);
+        
         sessionStorage.setItem('hqs_items', JSON.stringify(data));
     }
     
+    const updateSearch = (event) => {
+        setSearch(event.target.value.substr(0, 20));
+
+        setTest(backMsg.filter(
+            (item) => {
+                return item.title.toLowerCase().indexOf(search) !== -1;
+            }
+        ));
+
+        console.log('search: ', search);
+        console.log('data: ', test);
+        
+        
+    }
+
+
     useEffect (() => {
 
         const sessionStorageVar = sessionStorage.getItem('hqs_items') ? sessionStorage.getItem('hqs_items') : null;
 
         if(sessionStorageVar == null) {
-            console.log('hq ta vazia');
-            
             callApi();
         } else {
-            console.log('já tenho HQ po');
             setBackMsg(JSON.parse(sessionStorageVar));
+            setTest(JSON.parse(sessionStorageVar));
+            
             
         }
-
-
-
     }, [])
 
     return (
+        <>
         <div>
-            <Search />           
+            <input type="search" 
+                className="inp"
+                onChange={updateSearch} 
+                value={search}
+            />
+
             <div className="hq-list">
-                {backMsg.map((response) => {     
+                {test.map((response) => {     
                     return(
                         <Item 
                             title={response.title}
@@ -49,6 +68,7 @@ const Home = () => {
                 })}
             </div>
         </div>
+    </>
     )
 }
         export default Home;
